e595554e8161810da118dce9e3a484a8801a0e7c
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-07 01:36:58 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -3,15 +3,15 @@
 #include "allegro5/allegro_native_dialog.h"
 
 int main(int argc, char **argv) {
-
-   
+       
     ALLEGRO_DISPLAY *display = NULL;
     ALLEGRO_BITMAP  *menu1 = NULL;
     ALLEGRO_BITMAP  *menu2 = NULL;
     ALLEGRO_BITMAP  *menu3 = NULL;
-    ALLEGRO_EVENT_SOURCE *mouse = NULL;
+   ALLEGRO_EVENT_QUEUE *event_queue= al_create_event_queue();
     bool salir = false;
-    
+    int x, y;
+
     if (!al_init()) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
@@ -21,14 +21,16 @@ int main(int argc, char **argv) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
     }
+
     al_install_mouse;
     display = al_create_display(898, 798);
-   
+
     if (!display) {
-	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+	   al_show_native_message_box(display, "Error", "Error", "failed to create event_queue!\n", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
     }
-    mouse = al_get_mouse_event_source;
+
+    al_register_event_source(event_queue, al_get_mouse_event_source());
     menu1 = al_load_bitmap("menu1.png");
     menu2 = al_load_bitmap("menu2.png");
     menu3 = al_load_bitmap("menu3.png");
@@ -40,32 +42,38 @@ int main(int argc, char **argv) {
 	   return 0;
     }
     
-    al_draw_bitmap(menu1, 0, 0, 0);
-
     al_flip_display();
     al_rest(2);
 
     while (!salir) {
-	   if (mouse.x(ALLEGRO_EVENT_MOUSE_AXES) > 222 && mouse.x(ALLEGRO_EVENT_MOUSE_AXES) < 536 && 
-		  mouse.y(ALLEGRO_EVENT_MOUSE_AXES)> 515 && mouse.y(ALLEGRO_EVENT_MOUSE_AXES) < 586) {
-
-		  al_draw_bitmap(menu2, 0, 0, 0);
-		  salir = true;
+	   ALLEGRO_EVENT ev;
+	   
+	   if (ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
+		  x = ev.mouse.x;
+		  y = ev.mouse.y;
 	   }
 
-	   else if (mouse.x(ALLEGRO_EVENT_MOUSE_AXES) > 239 && mouse.x(ALLEGRO_EVENT_MOUSE_AXES) < 627 && 
-		  mouse.y(ALLEGRO_EVENT_MOUSE_AXES) > 411 && mouse.y(ALLEGRO_EVENT_MOUSE_AXES) < 692) {
+
+	   if (x> 222 && x < 536 && y> 515 && y < 586) {
+		  al_draw_bitmap(menu2, 0, 0, 0);
+		  if (ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			 salir = true;}
+		  
+	   }
+	   else if (x > 239 && x < 627 &&  y > 411 && y < 692) {
 	       al_draw_bitmap(menu3, 0, 0, 0);
-		  salir = true;
+		  if (ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			 salir = true;
+		  }
 	   }
 
 	   else {
 		  al_draw_bitmap(menu1, 0, 0, 0);}
     }
 
-
     al_destroy_display(display);
     al_destroy_bitmap(menu1);
+    al_destroy_event_queue(event_queue);
 
     return 0;
 }
