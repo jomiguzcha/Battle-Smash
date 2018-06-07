b774bb5a482aadaeab9e782b196ad97765c72d1c
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-07 06:47:35 -0500

    Update Sprite.h

--- Sprite.h
+++ Sprite.h
@@ -1,1 +1,20 @@
-#pragma once
+#include <string>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+class Sprite
+{
+protected:
+	ALLEGRO_BITMAP *bitmap;
+	int x, y;
+
+public:
+	Sprite();
+	virtual ~Sprite();
+
+	void setBitmap(std::string filePath);
+	ALLEGRO_BITMAP *getBitmap();
+
+	void draw();
+};
+
