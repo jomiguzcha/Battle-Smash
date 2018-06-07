df53a1e55968591a67eff9bfd8f5e3393a95388b
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-07 06:48:37 -0500

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,1 +1,23 @@
+#include "Player.h"
 
+Player::Player()
+{
+	health = 0;
+	moveSpeed = 5;
+}
+
+Player::~Player()
+{
+}
+
+void Player::doLogic(Keyboard keyboard)
+{
+	if (keyboard.key[UP])
+		y -= moveSpeed;
+	else if (keyboard.key[DOWN])
+		y += moveSpeed;
+	if (keyboard.key[LEFT])
+		x -= moveSpeed;
+	else if (keyboard.key[RIGHT])
+		x += moveSpeed;
+}
