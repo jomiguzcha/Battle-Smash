cc8e5b3021e82a5d0a93cd651291765f5d0359bb
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-07 07:50:46 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,11 +1,17 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
+#include "Player.h"
+
 class Game {
 private:
-	ALLEGRO_DISPLAY * display;
+	ALLEGRO_DISPLAY *display;
 	ALLEGRO_TIMER *timer;
 	ALLEGRO_EVENT_QUEUE *event_queue;
+	ALLEGRO_BITMAP  *image ;
+	
+	Player player;
+
 
 	bool loop, redraw;
 public:
@@ -18,4 +24,3 @@ public:
 
 
 };
-
