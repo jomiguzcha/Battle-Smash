f41e182805654e672da01bf606eec84cbdaba693
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-06 23:35:17 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+
+class Game {
+private:
+	ALLEGRO_DISPLAY * display;
+	ALLEGRO_TIMER *timer;
+	ALLEGRO_EVENT_QUEUE *event_queue;
+
+	bool loop, redraw;
+public:
+	Game();
+	~Game();
+
+	int init();
+	int createWindow(float , int , int );
+	void gameLoop();
+
+
+};
+
