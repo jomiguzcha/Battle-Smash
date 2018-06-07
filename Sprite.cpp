f14ed8696ddfce7f2498ce6ef6a49ffa0d3ab223
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-07 06:46:46 -0500

    Update Sprite.cpp

--- Sprite.cpp
+++ Sprite.cpp
@@ -1,1 +1,27 @@
+#include "Sprite.h"
 
+Sprite::Sprite()
+{
+	bitmap = NULL;
+	x = 0;
+	y = 0;
+}
+
+Sprite::~Sprite()
+{
+}
+
+void Sprite::setBitmap(std::string filePath)
+{
+	bitmap = al_load_bitmap(filePath.c_str());
+}
+
+ALLEGRO_BITMAP *Sprite::getBitmap()
+{
+	return bitmap;
+}
+
+void Sprite::draw()
+{
+	al_draw_bitmap(bitmap, x, y, NULL);
+}
