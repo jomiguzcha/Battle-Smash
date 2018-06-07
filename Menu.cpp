6591f4f58b310323b150c31e546f1e3e00380029
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-07 00:05:24 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -4,14 +4,14 @@
 
 int main(int argc, char **argv) {
 
+    al_install_mouse;
     ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_BITMAP  *buffer= NULL;
     ALLEGRO_BITMAP  *menu1 = NULL;
     ALLEGRO_BITMAP  *menu2 = NULL;
     ALLEGRO_BITMAP  *menu3 = NULL;
-    ALLEGRO_EVENT_MOUSE_AXES;
+    ALLEGRO_EVENT_SOURCE *al_get_mouse_event_source(mouse);
     bool salir = false;
-
+    
     if (!al_init()) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
@@ -21,33 +21,46 @@ int main(int argc, char **argv) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
     }
-    
-    al_install_mouse;
-    display = al_create_display(898, 798);
 
-    if (!display) { 
+    display = al_create_display(898, 798);
+   
+    if (!display) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   return 0;
     }
 
-    buffer = al_create_bitmap(898, 798);
-    menu1= al_load_bitmap("menu1.png");
+    menu1 = al_load_bitmap("menu1.png");
     menu2 = al_load_bitmap("menu2.png");
     menu3 = al_load_bitmap("menu3.png");
+
 
     if (!menu1) {
 	   al_show_native_message_box(display, "Error", "Error", "Failed to load image!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
 	   al_destroy_display(display);
 	   return 0;
     }
-
+    
     al_draw_bitmap(menu1, 0, 0, 0);
 
     al_flip_display();
     al_rest(2);
 
     while (!salir) {
-		  if(ALLEGRO_EVENT_MOUSE_AXES)
+	   if (mouse.x(ALLEGRO_EVENT_MOUSE_AXES) > 222 && mouse.x(ALLEGRO_EVENT_MOUSE_AXES) < 536 && 
+		  mouse.y(ALLEGRO_EVENT_MOUSE_AXES)> 515 && mouse.y(ALLEGRO_EVENT_MOUSE_AXES) < 586) {
+
+		  al_draw_bitmap(menu2, 0, 0, 0);
+		  salir = true;
+	   }
+
+	   else if (mouse.x(ALLEGRO_EVENT_MOUSE_AXES) > 239 && mouse.x(ALLEGRO_EVENT_MOUSE_AXES) < 627 && 
+		  mouse.y(ALLEGRO_EVENT_MOUSE_AXES) > 411 && mouse.y(ALLEGRO_EVENT_MOUSE_AXES) < 692) {
+	       al_draw_bitmap(menu3, 0, 0, 0);
+		  salir = true;
+	   }
+
+	   else {
+		  al_draw_bitmap(menu1, 0, 0, 0);}
     }
 
 
