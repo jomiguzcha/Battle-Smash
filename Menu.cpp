00a5e204801030781645471b5cd4c4cd51411570
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-13 09:40:30 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -3,54 +3,30 @@
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_native_dialog.h"
 
-using namespace std;
+Menu(){
+    menu1=NULL;
+    menu2=NULL;
+    menu3=NULL;
+    salir=false ;x=0; y=0;
+}
 
-int main(int argc, char **argv) {
-       
-    ALLEGRO_DISPLAY *display = NULL;
-    ALLEGRO_BITMAP  *menu1 = NULL;
-    ALLEGRO_BITMAP  *menu2 = NULL;
-    ALLEGRO_BITMAP  *menu3 = NULL;
-   ALLEGRO_EVENT_QUEUE *event_queue= NULL;
-    bool salir = false;
-    int x, y;
-
-    if (!al_init()) {
-	   return 0;
+~Menu(){
+    al_destroy_bitmap(menu1);
+    al_destroy_bitmap(menu2);
+    al_destroy_bitmap(menu3);
     }
 
-    if (!al_init_image_addon()) {
-	   return 0;
-    }
-    
-    al_install_mouse();
-
-    display = al_create_display(898, 798);
-
-    if (!display) {
-	   return 0;
-    }
-
-    event_queue = al_create_event_queue();
-
-    if (!event_queue) {
-	   al_destroy_event_queue(event_queue);
-	   al_destroy_display(display);
-	   return 0;
-    }
-
-
-    al_register_event_source(event_queue, al_get_display_event_source(display));
-    al_register_event_source(event_queue, al_get_mouse_event_source());
-    menu1 = al_load_bitmap("menu1.png");    
+int loadWindow(){
+    menu1 = al_load_bitmap("menu1.png");
     menu2 = al_load_bitmap("menu2.png");
     menu3 = al_load_bitmap("menu3.png");
- 
+}
 
-    while (!salir) {
+void Loop(){
+   while (!salir) {
 	   ALLEGRO_EVENT ev;
 	   al_wait_for_event(event_queue, &ev);
-	   
+
 	   if (ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
 		  x = ev.mouse.x;
 		  y = ev.mouse.y;
@@ -62,7 +38,7 @@ int main(int argc, char **argv) {
 		  al_draw_bitmap(menu2, 0, 0, 0);
 		  if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
 			 salir = true;}
-		  
+
 	   }
 	   else if (x > 239 && x < 627 &&  y > 411 && y < 692) {
 	       al_draw_bitmap(menu3, 0, 0, 0);
@@ -75,10 +51,4 @@ int main(int argc, char **argv) {
 		  al_flip_display();
 		  al_rest(2); }
     }
-
-    al_destroy_display(display);
-    al_destroy_bitmap(menu1);
-    al_destroy_event_queue(event_queue);
-
-    return 0;
 }
