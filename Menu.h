f43452543173eafd6128511deee23f6928fd18c3
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-17 22:41:20 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -1,25 +1,29 @@
-#ifndef MENU_H
-#define MENU_H
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
 
 class Menu
 {   private:
-        ALLEGRO_DISPLAY *display;
-        ALLEGRO_BITMAP  *menu1;
-        ALLEGRO_BITMAP  *menu2;
-        ALLEGRO_BITMAP  *menu3;
-        ALLEGRO_EVENT_QUEUE *event_queue;
-        bool salir ;int x, y;
+		  ALLEGRO_DISPLAY * display ;
+		  ALLEGRO_BITMAP  *menu ;   
+		  ALLEGRO_BITMAP  *menu1;
+		  ALLEGRO_BITMAP  *menu2;
+		  ALLEGRO_BITMAP  *menu3;
+		  ALLEGRO_BITMAP  *menu4;
+		  ALLEGRO_BITMAP  *menu5;
+		  ALLEGRO_BITMAP  *menu6;
+		  ALLEGRO_BITMAP  *menu7;
+		  ALLEGRO_BITMAP  *menu8;
+		  ALLEGRO_EVENT_QUEUE *event_queue;
+		  int  x,ancho,alto;
 
     public:
-        Menu();
-        ~Menu();
+		  Menu();
+		  ~Menu();
 
-        int loadWindow();
-        void Loop();
-   
-};
+		  int init();
+		  int createWindow();
+		  void Loop();
 
-#endif // MENU_H
+	   };
+
