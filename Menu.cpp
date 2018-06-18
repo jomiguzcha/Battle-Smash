de35c2db1e69637fb4bb0c5ed6005a4af3360b4f
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-17 19:38:05 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -1,54 +1,174 @@
-#include <iostream>
 #include "allegro5/allegro.h"
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_native_dialog.h"
+#include "Menu.h"
 
-Menu(){
-    menu1=NULL;
-    menu2=NULL;
-    menu3=NULL;
-    salir=false ;x=0; y=0;
-}
+int main(int argc, char **argv) {
 
-~Menu(){
-    al_destroy_bitmap(menu1);
-    al_destroy_bitmap(menu2);
-    al_destroy_bitmap(menu3);
+    ALLEGRO_DISPLAY *display = NULL;
+    ALLEGRO_BITMAP  *menu = NULL;
+    ALLEGRO_BITMAP  *menu1 = NULL;
+    ALLEGRO_BITMAP  *menu2 = NULL;
+    ALLEGRO_BITMAP  *menu3 = NULL;
+    ALLEGRO_BITMAP  *menu4 = NULL;
+    ALLEGRO_BITMAP  *menu5 = NULL;
+    ALLEGRO_BITMAP  *menu6= NULL;
+    ALLEGRO_BITMAP  *menu7= NULL;
+    ALLEGRO_BITMAP  *menu8 = NULL;
+    ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+    int  x = 0;
+
+    if (!al_init()) {
+	   return 0;
     }
 
-int loadWindow(){
-    menu1 = al_load_bitmap("menu1.png");
-    menu2 = al_load_bitmap("menu2.png");
-    menu3 = al_load_bitmap("menu3.png");
-}
+    if (!al_init_image_addon()) {
+	    return 0;
+    }
 
-void Loop(){
-   while (!salir) {
+    al_install_keyboard();
+    display = al_create_display(898, 798);
+
+    if (!display) {
+	    return 0;
+    }
+
+    event_queue = al_create_event_queue();
+
+    if (!event_queue) {
+	   al_destroy_event_queue(event_queue);
+	   al_destroy_display(display);
+	   return 0;
+    }
+
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_keyboard_event_source());
+    menu = al_load_bitmap("second1.png");
+    menu1 = al_load_bitmap("jigly.png");
+    menu2 = al_load_bitmap("kirby.png");
+    menu3 = al_load_bitmap("link.png");	
+    menu4 = al_load_bitmap("tychus.png");
+    menu5 = al_load_bitmap("jigly2.png");
+    menu6 = al_load_bitmap("kirby2.png");
+    menu7 = al_load_bitmap("link2.png");
+    menu8 = al_load_bitmap("tychus2.png");
+
+    if (!menu1) {
+	   al_destroy_display(display);
+	   return 0;
+    }
+
+    if (!menu2) {
+	   al_destroy_display(display);
+	   return 0;
+    }
+
+    if (!menu3) {
+	   al_destroy_display(display);
+	   return 0;
+    }
+
+    if (!menu4) {
+	   al_destroy_display(display);
+	   return 0;
+    }
+
+    al_draw_bitmap(menu, 0, 0, 0);
+    al_draw_bitmap(menu1, 222, 214, 0);
+    al_draw_bitmap(menu2, 463, 214, 0);
+    al_draw_bitmap(menu3, 222, 490, 0);
+    al_draw_bitmap(menu4, 462, 492, 0);
+
+    al_flip_display();
+    al_rest(2);
+
+    while (x!=ALLEGRO_KEY_ENTER) {
 	   ALLEGRO_EVENT ev;
 	   al_wait_for_event(event_queue, &ev);
 
-	   if (ev.type == ALLEGRO_EVENT_MOUSE_AXES) {
-		  x = ev.mouse.x;
-		  y = ev.mouse.y;
-	   }
+	   if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		  x = ev.keyboard.keycode;}
 
-	   cout << "x: " << x <<"\t"<< "y: " << y << endl;
-
-	   if (x> 222 && x < 536 && y> 515 && y < 586) {
-		  al_draw_bitmap(menu2, 0, 0, 0);
-		  if (ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			 salir = true;}
-
-	   }
-	   else if (x > 239 && x < 627 &&  y > 411 && y < 692) {
-	       al_draw_bitmap(menu3, 0, 0, 0);
-		  if (ev.type==ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			 salir = true; }
-	   }
-
-	   else {
-		  al_draw_bitmap(menu1, 0, 0, 0);
+	   if (x==ALLEGRO_KEY_DOWN ) {
+		  x = 0;
+		  al_draw_bitmap(menu5, 222, 214, 0);
 		  al_flip_display();
-		  al_rest(2); }
+
+			 while (true) {
+				   al_wait_for_event(event_queue, &ev);
+				    if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+					   x = ev.keyboard.keycode; }
+
+				    if (x == ALLEGRO_KEY_DOWN) {
+					   al_draw_bitmap(menu1, 222, 214, 0);
+					   al_draw_bitmap(menu7, 222, 490, 0);
+					   al_flip_display();
+					   al_rest(2);
+					   x = 0;
+					   break;
+				     }
+
+				    else if (x == ALLEGRO_KEY_RIGHT) {
+					   al_draw_bitmap(menu1, 222, 214, 0);
+					   al_draw_bitmap(menu6, 463, 214, 0);
+					   al_flip_display();
+					   al_rest(2);
+					   x = 0;
+					   break;
+					   }
+
+				    else if (x == ALLEGRO_KEY_ENTER) {
+					   break;}
+			   }
+	   }
+
+	   else if (x == ALLEGRO_KEY_RIGHT) {
+		  x = 0;
+		  al_draw_bitmap(menu6, 463, 214, 0);
+		  al_flip_display();
+
+			 while (true) {
+					   al_wait_for_event(event_queue, &ev);
+					   if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+						  x = ev.keyboard.keycode;
+					   }
+
+					   if (x == ALLEGRO_KEY_DOWN) {
+						  al_draw_bitmap(menu2, 463, 214, 0);
+						  al_draw_bitmap(menu8, 462, 492, 0);
+						  al_flip_display();
+						  al_rest(2);
+						  x = 0;
+						  break;
+					   }
+
+					   else if (x == ALLEGRO_KEY_LEFT) {
+						  al_draw_bitmap(menu2, 463, 214, 0);
+						  al_draw_bitmap(menu5, 222, 214, 0);
+						  al_flip_display();
+						  al_rest(2);
+						  x = 0;
+						  break;
+					   }
+
+					   else if (x == ALLEGRO_KEY_ENTER) {
+						  break;
+					   }
+			 }
+	   }
     }
+
+
+    al_destroy_display(display);
+    al_destroy_bitmap(menu1);
+    al_destroy_bitmap(menu2);
+    al_destroy_bitmap(menu3);
+    al_destroy_bitmap(menu4);
+    al_destroy_bitmap(menu5);
+    al_destroy_bitmap(menu6);
+    al_destroy_bitmap(menu7);
+    al_destroy_bitmap(menu8);
+    al_destroy_event_queue(event_queue);
+
+    return 0;
 }
