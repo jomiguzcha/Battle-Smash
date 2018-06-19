9cd77a15736ff0a9ac1a0a7fe97cc33691a0377d
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-18 23:25:17 -0500

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -5,29 +5,44 @@
 
 Menu::Menu(){
 	   display = NULL;
-	   menu = NULL;
-	   menu1 = NULL;
+	   menu1_1 = NULL;
+	   menu1_2 = NULL;
+	   menu1_3 = NULL;
 	   menu2 = NULL;
-	   menu3 = NULL;
-	   menu4 = NULL;
-	   menu5 = NULL;
-	   menu6 = NULL;
-	   menu7 = NULL;
-	   menu8 = NULL;
+	   jigly1 = NULL;
+	   kirby1 = NULL;
+	   link1 = NULL;
+	   tychus1 = NULL;
+	   jigly2 = NULL;
+	   kirby2 = NULL;
+	   link2 = NULL;
+	   tychus2 = NULL;
+	   jigly3 = NULL;
+	   kirby3 = NULL;
+	   link3 = NULL;
+	   tychus3 = NULL;
 	   event_queue = NULL;
-	   x = 0; ancho = 898; alto = 798;
+	   x = 0; y = 0; ancho = 898; alto = 798;
 }
 
 Menu::~Menu(){
     al_destroy_display(display);
-    al_destroy_bitmap(menu1);
+    al_destroy_bitmap(menu1_1);
+    al_destroy_bitmap(menu1_2);
+    al_destroy_bitmap(menu1_3);
     al_destroy_bitmap(menu2);
-    al_destroy_bitmap(menu3);
-    al_destroy_bitmap(menu4);
-    al_destroy_bitmap(menu5);
-    al_destroy_bitmap(menu6);
-    al_destroy_bitmap(menu7);
-    al_destroy_bitmap(menu8);
+    al_destroy_bitmap(jigly1);
+    al_destroy_bitmap(kirby1);
+    al_destroy_bitmap(link1);
+    al_destroy_bitmap(tychus1);
+    al_destroy_bitmap(jigly2);
+    al_destroy_bitmap(kirby2);
+    al_destroy_bitmap(link2);
+    al_destroy_bitmap(tychus2);
+    al_destroy_bitmap(jigly3);
+    al_destroy_bitmap(kirby3);
+    al_destroy_bitmap(link3);
+    al_destroy_bitmap(tychus3);
     al_destroy_event_queue(event_queue);
 }
 
@@ -39,13 +54,14 @@ int Menu::init() {
 int Menu::createWindow() {
     display = al_create_display(ancho, alto);
 
-    if (!display){
+    if (!display) {
 	   al_destroy_display(display);
-	   return 0;}
+	   return 0;
+    }
 
     event_queue = al_create_event_queue();
 
-    if (!event_queue){
+    if (!event_queue) {
 	   al_destroy_event_queue(event_queue);
 	   al_destroy_display(display);
 	   return 0;
@@ -53,52 +69,59 @@ int Menu::createWindow() {
 
     al_init_image_addon();
     al_install_keyboard();
-    menu = al_load_bitmap("second1.png");
-    menu1 = al_load_bitmap("jigly.png");
-    menu2 = al_load_bitmap("kirby.png");
-    menu3 = al_load_bitmap("link.png");
-    menu4 = al_load_bitmap("tychus.png");
-    menu5 = al_load_bitmap("jigly2.png");
-    menu6 = al_load_bitmap("kirby2.png");
-    menu7 = al_load_bitmap("link2.png");
-    menu8 = al_load_bitmap("tychus2.png");
+    menu1_1 = al_load_bitmap("menu1.png");
+    menu1_2 = al_load_bitmap("menu2.png");
+    menu1_3= al_load_bitmap("menu3.png");
+    menu2 = al_load_bitmap("second1.png");
+    jigly1 = al_load_bitmap("jigly.png");
+    kirby1 = al_load_bitmap("kirby.png");
+    link1 = al_load_bitmap("link.png");
+    tychus1 = al_load_bitmap("tychus.png");
+    jigly2 = al_load_bitmap("jigly2.png");
+    kirby2 = al_load_bitmap("kirby2.png");
+    link2 = al_load_bitmap("link2.png");
+    tychus2 = al_load_bitmap("tychus2.png");
+    jigly3 = al_load_bitmap("jigly3.png");
+    kirby3 = al_load_bitmap("kirby3.png");
+    link3 = al_load_bitmap("link3.png");
+    tychus3 = al_load_bitmap("tychus3.png");
 
-    if (!menu1) {
+    if (!jigly1) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu2) {
+    if (!kirby1) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu3) {
+    if (!link1) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu4) {
+    if (!tychus1) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu5) {
+    if (!jigly2) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu6) {
+    if (!kirby2) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu7) {
+    if (!link2) {
 	   al_destroy_display(display);
 	   return 0;
     }
 
-    if (!menu8) {
+    if (!tychus2) {
 	   al_destroy_display(display);
 	   return 0;
     }
@@ -106,20 +129,76 @@ int Menu::createWindow() {
     al_register_event_source(event_queue, al_get_display_event_source(display));
     al_register_event_source(event_queue, al_get_keyboard_event_source());
 
-    al_draw_bitmap(menu, 0, 0, 0);
-    al_draw_bitmap(menu1, 222, 214, 0);
-    al_draw_bitmap(menu2, 463, 214, 0);
-    al_draw_bitmap(menu3, 222, 490, 0);
-    al_draw_bitmap(menu4, 462, 492, 0);
-
-    al_flip_display();
-    al_rest(10);
-
     return 0;
 }
 
+void Menu::draw_menu1() {
+	   al_draw_bitmap(menu1_1, 0, 0, 0);
+	   al_flip_display();
+	   al_rest(2);
+}
 
-void Menu::Loop() {
+void Menu::draw_menu2() {
+	   al_draw_bitmap(menu2, 0, 0, 0);
+	   al_draw_bitmap(jigly1, 222, 214, 0);
+	   al_draw_bitmap(kirby1, 463, 214, 0);
+	   al_draw_bitmap(link1, 222, 490, 0);
+	   al_draw_bitmap(tychus1, 462, 492, 0);
+
+	   al_flip_display();
+	   al_rest(2);
+}
+
+void Menu::Loop(){
+		  while (y!= ALLEGRO_KEY_ENTER) {
+				ALLEGRO_EVENT ev;
+				al_wait_for_event(event_queue, &ev);
+
+				if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+				    y = ev.keyboard.keycode;
+				}
+
+				if (y == ALLEGRO_KEY_ENTER) {
+				    break;
+				}
+
+				else if (y == ALLEGRO_KEY_DOWN) {
+				    y = 0;
+				    al_draw_bitmap(menu1_2, 0, 0, 0);
+				    al_flip_display();
+				    al_rest(2);
+
+				    while (true) {
+							 al_wait_for_event(event_queue, &ev);
+							 if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+								y = ev.keyboard.keycode; 
+							 }
+
+							 if (y == ALLEGRO_KEY_DOWN) {
+								y = 0;
+								al_draw_bitmap(menu1_3, 0, 0, 0);
+								al_flip_display();
+								al_rest(2);
+								break;
+							 }
+
+							 else if (y == ALLEGRO_KEY_ENTER) {
+								break;
+							 }
+				     }
+				}
+
+				else if (y == ALLEGRO_KEY_UP) {
+				    y = 0;
+				    al_draw_bitmap(menu2, 0, 0, 0);
+				    al_flip_display();
+				    al_rest(2);
+				}
+		  }
+}
+
+
+void Menu::Loop2() {
     while (x != ALLEGRO_KEY_ENTER) {
 	   ALLEGRO_EVENT ev;
 	   al_wait_for_event(event_queue, &ev);
@@ -130,8 +209,9 @@ void Menu::Loop() {
 
 	   if (x == ALLEGRO_KEY_DOWN) {
 		  x = 0;
-		  al_draw_bitmap(menu5, 222, 214, 0);
+		  al_draw_bitmap(jigly2, 222, 214, 0);
 		  al_flip_display();
+		  al_rest(2);
 
 		  while (true) {
 			 al_wait_for_event(event_queue, &ev);
@@ -140,8 +220,8 @@ void Menu::Loop() {
 			 }
 
 			 if (x == ALLEGRO_KEY_DOWN) {
-				al_draw_bitmap(menu1, 222, 214, 0);
-				al_draw_bitmap(menu7, 222, 490, 0);
+				al_draw_bitmap(jigly1, 222, 214, 0);
+				al_draw_bitmap(link2, 222, 490, 0);
 				al_flip_display();
 				al_rest(2);
 				x = 0;
@@ -149,8 +229,8 @@ void Menu::Loop() {
 			 }
 
 			 else if (x == ALLEGRO_KEY_RIGHT) {
-				al_draw_bitmap(menu1, 222, 214, 0);
-				al_draw_bitmap(menu6, 463, 214, 0);
+				al_draw_bitmap(jigly1, 222, 214, 0);
+				al_draw_bitmap(kirby2, 463, 214, 0);
 				al_flip_display();
 				al_rest(2);
 				x = 0;
@@ -165,8 +245,9 @@ void Menu::Loop() {
 
 	   else if (x == ALLEGRO_KEY_RIGHT) {
 		  x = 0;
-		  al_draw_bitmap(menu6, 463, 214, 0);
+		  al_draw_bitmap(kirby2, 463, 214, 0);
 		  al_flip_display();
+		  al_rest(2);
 
 		  while (true) {
 			 al_wait_for_event(event_queue, &ev);
@@ -175,8 +256,8 @@ void Menu::Loop() {
 			 }
 
 			 if (x == ALLEGRO_KEY_DOWN) {
-				al_draw_bitmap(menu2, 463, 214, 0);
-				al_draw_bitmap(menu8, 462, 492, 0);
+				al_draw_bitmap(kirby1, 463, 214, 0);
+				al_draw_bitmap(tychus2, 462, 492, 0);
 				al_flip_display();
 				al_rest(2);
 				x = 0;
@@ -184,8 +265,89 @@ void Menu::Loop() {
 			 }
 
 			 else if (x == ALLEGRO_KEY_LEFT) {
-				al_draw_bitmap(menu2, 463, 214, 0);
-				al_draw_bitmap(menu5, 222, 214, 0);
+				al_draw_bitmap(kirby1, 463, 214, 0);
+				al_draw_bitmap(jigly2, 222, 214, 0);
+				al_flip_display();
+				al_rest(2);
+				x = 0;
+				break;
+			 }
+
+			 else if (x == ALLEGRO_KEY_ENTER) {
+				break;
+			 }
+		  }
+	   }
+    }
+    x = 0;
+    while (x != ALLEGRO_KEY_ENTER) {
+	   ALLEGRO_EVENT ev;
+	   al_wait_for_event(event_queue, &ev);
+
+	   if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		  x = ev.keyboard.keycode;
+	   }
+
+	   if (x == ALLEGRO_KEY_DOWN) {
+		  x = 0;
+		  al_draw_bitmap(jigly3, 222, 214, 0);
+		  al_flip_display();
+		  al_rest(2);
+
+		  while (true) {
+			 al_wait_for_event(event_queue, &ev);
+			 if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+				x = ev.keyboard.keycode;
+			 }
+
+			 if (x == ALLEGRO_KEY_DOWN) {
+				al_draw_bitmap(jigly1, 222, 214, 0);
+				al_draw_bitmap(link3, 222, 490, 0);
+				al_flip_display();
+				al_rest(2);
+				x = 0;
+				break;
+			 }
+
+			 else if (x == ALLEGRO_KEY_RIGHT) {
+				al_draw_bitmap(jigly1, 222, 214, 0);
+				al_draw_bitmap(kirby3, 463, 214, 0);
+				al_flip_display();
+				al_rest(2);
+				x = 0;
+				break;
+			 }
+
+			 else if (x == ALLEGRO_KEY_ENTER) {
+				break;
+			 }
+		  }
+	   }
+
+	   else if (x == ALLEGRO_KEY_RIGHT) {
+		  x = 0;
+		  al_draw_bitmap(kirby3, 463, 214, 0);
+		  al_flip_display();
+		  al_rest(2);
+
+		  while (true) {
+			 al_wait_for_event(event_queue, &ev);
+			 if (ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+				x = ev.keyboard.keycode;
+			 }
+
+			 if (x == ALLEGRO_KEY_DOWN) {
+				al_draw_bitmap(kirby1, 463, 214, 0);
+				al_draw_bitmap(tychus3, 462, 492, 0);
+				al_flip_display();
+				al_rest(2);
+				x = 0;
+				break;
+			 }
+
+			 else if (x == ALLEGRO_KEY_LEFT) {
+				al_draw_bitmap(kirby1, 463, 214, 0);
+				al_draw_bitmap(jigly3, 222, 214, 0);
 				al_flip_display();
 				al_rest(2);
 				x = 0;
