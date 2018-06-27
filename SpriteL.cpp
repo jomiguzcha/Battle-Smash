2c77b2b9b43720247c716bcc9bc8f05d2cd476af
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-27 07:43:51 -0500

    Update SpriteL.cpp

--- SpriteL.cpp
+++ SpriteL.cpp
@@ -1,26 +1,26 @@
 #include "SpriteL.h"
 
 SpriteL::SpriteL(){
-    Player1=al_load_bitmap("1L.png");
-    Player2 = al_load_bitmap("2L.png");
-    Player3 = al_load_bitmap("3L.png");
-    Player4 = al_load_bitmap("4L.png");
-    Player5 = al_load_bitmap("5L.png");
-    Player6 = al_load_bitmap("6L.png");
-    Player7 = al_load_bitmap("7L.png");
-    Player8 = al_load_bitmap("8L.png");
-    Player9 = al_load_bitmap("9L.png");
-    Player10 = al_load_bitmap("10L.png");
-    Player11 = al_load_bitmap("11L.png");
-    Player12 = al_load_bitmap("12L.png");
-    Player13 = al_load_bitmap("13L.png");
-    Player14 = al_load_bitmap("14L.png");
-    Player15 = al_load_bitmap("15L.png");
-    Player16 = al_load_bitmap("16L.png"); 
-    Player17 = al_load_bitmap("17L.png");
-    Player18 = al_load_bitmap("18L.png");
-    Player19 = al_load_bitmap("19L.png");
-    Player20 = al_load_bitmap("20L.png");
+    Player1=al_load_bitmap("Link\1L.png");
+    Player2 = al_load_bitmap("Link\2L.png");
+    Player3 = al_load_bitmap("Link\3L.png");
+    Player4 = al_load_bitmap("Link\4L.png");
+    Player5 = al_load_bitmap("Link\5L.png");
+    Player6 = al_load_bitmap("Link\6L.png");
+    Player7 = al_load_bitmap("Link\7L.png");
+    Player8 = al_load_bitmap("Link\8L.png");
+    Player9 = al_load_bitmap("Link\9L.png");
+    Player10 = al_load_bitmap("Link\10L.png");
+    Player11 = al_load_bitmap("Link\11L.png");
+    Player12 = al_load_bitmap("Link\12L.png");
+    Player13 = al_load_bitmap("Link\13L.png");
+    Player14 = al_load_bitmap("Link\14L.png");
+    Player15 = al_load_bitmap("Link\15L.png");
+    Player16 = al_load_bitmap("Link\16L.png"); 
+    Player17 = al_load_bitmap("Link\17L.png");
+    Player18 = al_load_bitmap("Link\18L.png");
+    Player19 = al_load_bitmap("Link\19L.png");
+    Player20 = al_load_bitmap("Link\20L.png");
   
 }
 
