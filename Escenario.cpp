a1fb1d261beb36a8cd740d9abfadef42553f2aa7
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-27 06:14:45 -0500

    Update Escenario.cpp

--- Escenario.cpp
+++ Escenario.cpp
@@ -7,6 +7,10 @@ Escenario::Escenario(){
 
 void Escenario::pintar(){
     esc = al_load_bitmap("escenario.png");
+    if (!esc) {
+	   al_destroy_display(display);
+	   return 0;
+    }
     al_draw_bitmap(esc, 0, 0, 0);
     al_flip_display();
     al_rest(2);
