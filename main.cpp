8d30682aff4208a48652ef5af4eeeee36f3fb44b
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-26 23:08:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
 #include "Game.h"
-
 
 int main() {
 	Game *game = new Game();
 	game->init();
-	game->createWindow(60.0, 800, 600);
+	game->createWindow(60.0, 898,798);
+	game->loadWindow();
 	game->gameLoop();
-
+	
 	return 0;
 }
