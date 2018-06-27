c6627f3aa4aa4f2b73f0050e75b4c549c52c8db0
ALVARO CERNA <31244499+Alvarofrk@users.noreply.github.com>
2018-06-27 07:42:47 -0500

    Update SpriteK.cpp

--- SpriteK.cpp
+++ SpriteK.cpp
@@ -1,26 +1,26 @@
 #include "SpriteK.h"
 
 SpriteK::SpriteK(){
-    Player1=al_load_bitmap("1K.png");
-    Player2 = al_load_bitmap("2K.png");
-    Player3 = al_load_bitmap("3K.png");
-    Player4 = al_load_bitmap("4K.png");
-    Player5 = al_load_bitmap("5K.png");
-    Player6 = al_load_bitmap("6K.png");
-    Player7 = al_load_bitmap("7K.png");
-    Player8 = al_load_bitmap("8K.png");
-    Player9 = al_load_bitmap("9K.png");
-    Player10 = al_load_bitmap("10K.png");
-    Player11 = al_load_bitmap("11K.png");
-    Player12 = al_load_bitmap("12K.png");
-    Player13 = al_load_bitmap("13K.png");
-    Player14 = al_load_bitmap("14K.png");
-    Player15 = al_load_bitmap("15K.png");
-    Player16 = al_load_bitmap("16K.png"); 
-    Player17 = al_load_bitmap("17K.png");
-    Player18 = al_load_bitmap("18K.png");
-    Player19 = al_load_bitmap("19K.png");
-    Player20 = al_load_bitmap("20K.png");
+    Player1=al_load_bitmap("Kirby\1K.png");
+    Player2 = al_load_bitmap("Kirby\2K.png");
+    Player3 = al_load_bitmap("Kirby\3K.png");
+    Player4 = al_load_bitmap("Kirby\4K.png");
+    Player5 = al_load_bitmap("Kirby\5K.png");
+    Player6 = al_load_bitmap("Kirby\6K.png");
+    Player7 = al_load_bitmap("Kirby\7K.png");
+    Player8 = al_load_bitmap("Kirby\8K.png");
+    Player9 = al_load_bitmap("Kirby\9K.png");
+    Player10 = al_load_bitmap("Kirby\10K.png");
+    Player11 = al_load_bitmap("Kirby\11K.png");
+    Player12 = al_load_bitmap("Kirby\12K.png");
+    Player13 = al_load_bitmap("Kirby\13K.png");
+    Player14 = al_load_bitmap("Kirby\14K.png");
+    Player15 = al_load_bitmap("Kirby\15K.png");
+    Player16 = al_load_bitmap("Kirby\16K.png"); 
+    Player17 = al_load_bitmap("Kirby\17K.png");
+    Player18 = al_load_bitmap("Kirby\18K.png");
+    Player19 = al_load_bitmap("Kirby\19K.png");
+    Player20 = al_load_bitmap("Kirby\20K.png");
   
 }
 
