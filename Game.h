0d9f12fed958be1cbbcd0c37e2a54ece4a2b8b66
Axe1701 <37373371+Axe1701@users.noreply.github.com>
2018-06-27 06:42:11 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -9,7 +9,7 @@ protected:
 	ALLEGRO_DISPLAY *display;
 	ALLEGRO_TIMER *timer;
 	ALLEGRO_EVENT_QUEUE *event_queue;
-	ALLEGRO_SAMPLE *sample;
+	ALLEGRO_SAMPLES *sample;
 	bool loop, redraw;
 
 public:
